dab6141723e2063c4290ae3c6d1a3c49c8d7d2c7
janarild.eileng <janarild.eileng@fhi.no>
2023-01-11 15:38:18 +0100

    Oppgave11.sql

---
+++ oppgaver/oppgave-11/Oppgave11c.sql
@@ -0,0 +1,81 @@
+With
+Results_CTE as (
+select 
+   CONVERT(datetime2,substring(Date,7,4)+'-'+substring(Date,4,2)+'-'+substring(Date,1,2)) as  MatchDato,
+   HomeTeam,
+   AwayTeam,
+   CAST ( FTHG AS int ) as FTHG,
+   CAST ( FTAG AS int ) as FTAG
+from [Football].[dbo].[E02017] 
+),
+ResultsWithPoints_CTE as (
+select 
+  *,
+  CASE 
+	 When FTHG>FTAG THEN 3 
+	 When FTHG=FTAG THEN 1
+	 When FTHG<FTAG THEN 0
+  END  as HomePoints,
+  CASE 
+	 When FTHG>FTAG THEN 0 
+	 When FTHG=FTAG THEN 1
+	 When FTHG <FTAG THEN 3
+  END  as AwayPoints
+ FROM Results_CTE 
+),
+
+City_Teams_CTE As 
+(
+  Select 'Arsenal' as Team,'London' as City
+  union
+  Select 'Chelsea' as Team,'London' as City
+   union
+  Select 'Crystal Palace' as Team,'London' as City
+   union
+  Select 'Tottenham' as Team,'London' as City
+   union
+  Select 'Watford' as Team,'London' as City
+    union
+  Select 'West Ham' as Team,'London' as City
+   union
+  Select 'Man United' as Team,'Manchester' as City
+   union
+  Select 'Man City' as Team,'Manchester' as City
+   union
+  Select 'Liverpool' as Team,'Liverpool' as City
+    union
+  Select 'Everton' as Team,'Liverpool' as City
+
+
+
+
+),
+Only_LondonTeamMatches_CTE As 
+(
+  select *
+  from ResultsWithPoints_CTE r
+  where r.HomeTeam in ( select Team from City_Teams_CTE) and r.AwayTeam In (select Team from City_Teams_CTE)
+)
+
+
+
+select 
+  City,
+    Sum(TotalPoints) as TotalCityPoints
+  from
+(select 
+   Team, Sum(Points) as TotalPoints
+from (
+	  select 
+	     HomeTeam as Team , HomePoints as Points
+	  from Only_LondonTeamMatches_CTE
+	  Union all
+	  select 
+	     AwayTeam as Team , AwayPoints as Points
+	  from Only_LondonTeamMatches_CTE
+	) x
+GROUP BY Team
+) x
+JOIN City_Teams_CTE cityTeams on cityTeams.Team=x.Team
+GROUP BY City
+ORDER BY TotalCityPoints desc
